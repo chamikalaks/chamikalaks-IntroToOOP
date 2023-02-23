68571d25f2ee78e5aa45d5fc726a6c1989a0b44b
chamikalaks <chamikalaks@gmail.com>
2023-02-23 17:31:37 +0000

    lab oops

--- src/main/java/Bird.java
+++ src/main/java/Bird.java
@@ -1,3 +1,4 @@
+import org.junit.Test;
 
 /**
  * 
@@ -32,20 +33,31 @@ public class Bird {
      * A variable (in this context, often referred to as a field), which represents the age of the bird.
      */
     public int ageMonths;
+    
 
     /**
      * A constructor for the bird class: this method allows us to build a new bird with the 'new' keyword.
      * No need to change anything here, for now.
      */
-    public Bird(){
+    
+    public  Bird(){
 
     }
+     
+    
     /**
-     * @return "Tweet" if ageMonths is less than 3 and "Chirp" if ageMonths is
-     *         greater than or equal to 3.
+     * @return 
      */
-    public String sound(){
+    @Test
+    public String sound (){
+        if (ageMonths< 3){
+            return "Tweet";
+        }
+        if (ageMonths>3 || ageMonths == 3){
+            return "Chirp";}
         return "";
-    }
+        
+        }}
+    
 
-}
+    
